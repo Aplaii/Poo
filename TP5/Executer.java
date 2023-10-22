4afba73c6e910fc514329d4f22296258f6a09872
Aplaii <clem@bolifraud.fr>
2023-10-22 14:51:01 +0200

    a

--- TP5/Executer.java
+++ TP5/Executer.java
@@ -1,7 +1,18 @@
 package TP5;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Executer {
     
-    
+    private List<Property> props = new ArrayList<>();
+
+
+    public Executer(){
+        
+    }
+
+
+
+
 
 }
