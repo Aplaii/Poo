e2df53d2ee6b7fb6b7a6ed883cc46633e2546c9d
Aplaii <clem@bolifraud.fr>
2023-10-22 15:05:02 +0200

    TP5 Finished

--- TP5/Executer.java
+++ TP5/Executer.java
@@ -8,9 +8,63 @@ public class Executer {
 
 
     public Executer(){
-        
+
     }
 
+    public void addProperty(Property p){
+        this.props.add(p);
+    }
+
+    public List<Property> getProps(){
+        return this.props;
+    }
+
+    public void getProperty(int n){
+        System.out.println(this.props.get(n));
+    }
+
+    public int getNbVehicles(){
+        int nb = 0;
+        for(int i = 0; i < this.props.size(); i++){
+            if(this.props.get(i) instanceof Vehicle){
+                nb++;
+            }
+        }
+        return nb;
+    }
+
+    public int getMensualite(){
+        int v = 0;
+        for(int i = 0; i < this.props.size(); i++){
+            if(this.props.get(i) instanceof Housing){
+                v+= this.props.get(i).getCostByMonth();
+            }
+        }
+        return v;
+    }
+
+    public int getNbLoge(){
+        int v = 0;
+        for(int i = 0; i < this.props.size(); i++){
+            if(this.props.get(i) instanceof Housing){
+                Housing g = (Housing) this.props.get(i);
+                v+= g.getNbPersonnes();
+            }
+        }
+        return v;
+    }
+
+    public int getEntretienVehicle(){
+        int v = 0;
+        for(int i = 0; i < this.props.size(); i++){
+            if(this.props.get(i) instanceof Vehicle){
+                Vehicle g = (Vehicle) this.props.get(i);
+                v += g.getCostByMonth();
+            }
+        }
+        return v;
+    }
+    
 
 
 
