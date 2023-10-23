4840154a24b3ca3ae51b453d6da01089cdd0dcd2
Clement Bolifraud <22200144t>
2023-10-23 13:09:50 +0200

    Order files

---
+++ TP2/ex1/Polyligne.java
@@ -0,0 +1,67 @@
+package TP2;
+
+public class Polyligne {
+	Point [][] points;
+
+	public Polyligne(Point [][] p) {
+		this.points = p;
+	}
+		
+	
+	public Point [][] getPoints(){
+		return this.points;
+	}
+	
+	public void setPoints(Point [][] p) {
+		this.points = p;
+	}
+	
+	public int nbPoints() {
+		return points.length;
+	}
+	
+	public void translate(Vecteur v) {
+		for (int i = 0; i < points.length; i++) {
+			points[i][0].translater(v);
+		}
+	}
+	
+	public boolean polygone() {
+		
+		for (int i = 0; i < points.length-1; i++) {
+			if (points[i][1] != points[i+1][0]) {
+				return false;
+			}
+		}
+		return points[0][0] == points[-1][1];
+	}
+	
+	public void SurE () {
+		Point [] [] newlist = new Point [points.length*2][2];
+		for (int i = 0; i < (points.length)-1; i++) {
+			Point p0 = points[i][0];
+			Point p1 = points[i][1];
+			Point np = p0.moyen1(p1);
+			Point [] a = {p0, np};
+			Point [] b = {np, p1};
+			newlist[i+i] = a;
+			newlist[i+i+1] = b;
+		}
+		Point c = points[0][0].moyen1(points[-1][1]);
+		Point [] d = {points[-1][1], c};
+		Point [] e = {c, points[0][0]};
+		newlist[-2] = d;
+		newlist[-1] = e;
+	}
+	
+	public String toString() {
+		String res = "";
+		for (int i = 0; i < points.length; i++) {
+			res += "" + this.points[i] + " - ";
+		}
+		return res;
+	}
+	
+		
+	
+}
---
+++ TP2/ex2/CompteEpargne.java
@@ -0,0 +1,24 @@
+package TP2;
+
+public class CompteEpargne extends CompteBancaire{
+	
+	double interet = 0;
+	
+	public CompteEpargne(int g) {
+		super(g);
+	}
+	
+	public CompteEpargne(int g, int m) {
+		super(g, m);
+	}
+	
+	public void setInteret(int i) {
+		this.interet = i;
+	}
+	
+	public void calculInteret() {
+		this.setSolde(getSolde() * (1+interet/100));
+	}
+
+
+}
